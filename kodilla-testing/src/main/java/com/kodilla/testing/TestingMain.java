e9e9c5e31603129238656241420c974ec97e6862
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2020-10-08 23:06:41 +0200

    6.2 Added Calculator class and tested it

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+                String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+        Calculator calc = new Calculator();
+
+        Integer sum = calc.add(3, 5);
+        Integer difference = calc.subtract(5,7);
+
+        if(sum != null && difference != null) {
+                    System.out.println("Test OK" + " Sum = " + sum + " and difference = " + difference);
+        }else{
+            System.out.println("Error!");
+        }
+
     }
 }

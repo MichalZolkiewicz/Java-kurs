a9786d8bfbc8e71010667d314ff3fa158411cf89
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2020-10-25 19:05:12 +0100

    7.2 Added Classes and methods from this module

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -31,5 +32,8 @@ public class StreamMain {
         poemBeautifier.executeDecorate("This is text to decorate", text -> "ABC" + text + "ABC");
         poemBeautifier.executeDecorate("This is text to decorate", text -> text.toLowerCase());
         poemBeautifier.executeDecorate("This is text to decorate", text -> text + text);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }

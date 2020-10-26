4b032c54e8066d5319d72a844412fbfbda55aa0c
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2020-10-27 00:03:28 +0100

    7.5 Interface created and method tested in ArrayOperationsTestSuite

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    void testGetAverage() {
+
+        int[] numbers = {1,43,13,25,16,6,7,3,12,9};
+
+        assertEquals(ArrayOperations.getAverage(numbers), 13.5, 0);
+    }
+}

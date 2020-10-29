c12b41fd99d31ca2fc4fc87e33b374af3b72a920
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2020-10-29 20:16:19 +0100

    8.2 Try-catch-finally created for divide()

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModulRunner.java
@@ -0,0 +1,13 @@
+package com.kodilla.exception.main;
+
+import com.kodilla.exception.io.FileReader;
+
+public class ExceptionModulRunner {
+
+    public static void main(String[] args) {
+
+        FileReader fileReader = new FileReader();
+        fileReader.readFile();
+
+    }
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,33 @@
+package com.kodilla.exception.test;
+
+import java.sql.SQLOutput;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0){
+            throw new ArithmeticException();
+        }
+
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+            double result2 = firstChallenge.divide(3, 0);
+        }catch (ArithmeticException exception) {
+            System.out.println("Unfortunately I can't divide by 0. Error: " + exception);
+        }finally{
+            System.out.println("Math is not that complicated");
+        }
+
+
+    }
+}

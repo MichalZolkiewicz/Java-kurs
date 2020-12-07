59a6f0ee34250f4ebc0ea34783553399ccfcc70b
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2020-12-07 23:14:41 +0100

    14.2 Course materials modified

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootTest
 public class LibraryTestSuite {
 
@@ -15,12 +17,8 @@ public class LibraryTestSuite {
     @Test
     void testLoadFromDb() {
         //Given
-        //ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        //Library library = context.getBean(Library.class);
-
         //When
         library.loadFromDb();
-
         //Then
         //do nothing
     }
@@ -28,13 +26,21 @@ public class LibraryTestSuite {
     @Test
     void testSaveToDb() {
         //Given
-        //ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        //Library library = context.getBean(Library.class);
-
         //When
         library.saveToDb();
-
         //Then
         //do nothing
+    }
+
+    @Test
+    void testContext() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+        //When
+        System.out.println("====== Beans list: ====== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ====== Beans list: ======");
     }
 }

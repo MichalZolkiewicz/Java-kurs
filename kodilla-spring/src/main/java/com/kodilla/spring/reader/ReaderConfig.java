cc21c16a247013aec51faa79e572fe3242725991
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2020-12-09 20:53:46 +0100

    14 ReaderConfig class from module materials - unused import deleted.

--- kodilla-spring/src/main/java/com/kodilla/spring/reader/ReaderConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/reader/ReaderConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Repository;
+
 
 @Configuration
 public class ReaderConfig {

0b9517df248f8028b0f406d1da47d9484bc33702
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2020-12-09 20:52:42 +0100

    14.3 BoardConfig - unused import deleted, beans names amended to camelCase

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -2,7 +2,7 @@ package com.kodilla.spring.portfolio;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+
 
 @Configuration
 public class BoardConfig {
@@ -12,17 +12,17 @@ public class BoardConfig {
         return new Board(getListToDo(), getListInProgress(), getListDone());
     }
 
-    @Bean(name = "To do list")
+    @Bean(name = "toDoList")
     public TaskList getListToDo() {
         return new TaskList();
     }
 
-    @Bean(name = "In progress list")
+    @Bean(name = "inProgressList")
     public TaskList getListInProgress() {
         return new TaskList();
     }
 
-    @Bean(name = "Done list")
+    @Bean(name = "doneList")
     public TaskList getListDone() {
         return new TaskList();
     }

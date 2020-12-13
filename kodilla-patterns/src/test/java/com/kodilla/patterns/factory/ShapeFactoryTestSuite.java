2bd36913df48b8cbaf9711f30a7c18542f8302a7
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2020-12-13 16:00:59 +0100

    15.4 Module materials - factory pattern

--- kodilla-patterns/src/test/java/com/kodilla/patterns/factory/ShapeFactoryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/factory/ShapeFactoryTestSuite.java
@@ -3,7 +3,6 @@ package com.kodilla.patterns.factory;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ShapeFactoryTestSuite {
 

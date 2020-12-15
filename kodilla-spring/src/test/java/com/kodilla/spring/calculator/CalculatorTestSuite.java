11a55a743dc4630e872c2fd7e34c7cf1a525dec5
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2020-12-15 18:51:43 +0100

    14.2 @Autowired for Calculator field created in tests

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,17 +1,16 @@
 package com.kodilla.spring.calculator;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class CalculatorTestSuite {
 
-    ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-    Calculator calculator = context.getBean(Calculator.class);
+    @Autowired
+    Calculator calculator;
 
     @Test
     void testAddMethod() {

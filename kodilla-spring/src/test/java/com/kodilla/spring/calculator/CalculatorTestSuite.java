619e58368cb76e0b11bfb70e809d87063f16161b
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2020-12-15 14:14:48 +0100

    14.2 tests changed - 1 per each method.

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -10,25 +10,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class CalculatorTestSuite {
 
-    @Test
-    void testCalculations() {
-        //Given
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Calculator calculator = context.getBean(Calculator.class);
+    ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+    Calculator calculator = context.getBean(Calculator.class);
 
+    @Test
+    void testAddMethod() {
         //When
         double add = calculator.add(3,1);
-        double sub = calculator.sub(3,1);
-        double mul = calculator.mul(3,2);
-        double div = calculator.div(6,2);
-
-
-
         //Then
         assertEquals(4, add);
-        assertEquals(2, sub);
-        assertEquals(6, mul);
-        assertEquals(3, div);
+    }
 
+    @Test
+    void testSubMethod() {
+        //When
+        double sub = calculator.sub(3,1);
+        //Then
+        assertEquals(2, sub);
+    }
+
+    @Test
+    void testMulMethod() {
+        //When
+        double mul = calculator.mul(3,2);
+        //Then
+        assertEquals(6, mul);
+    }
+
+    @Test
+    void testDivMethod() {
+        //When
+        double div = calculator.div(6,2);
+        //Then
+        assertEquals(3, div);
     }
 }

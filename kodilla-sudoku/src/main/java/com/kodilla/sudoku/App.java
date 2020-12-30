250081d48f00d09ddd542309803ea11bd49b8c33
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2020-12-30 16:12:56 +0100

    17.2 Hibernate framework connection

---
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/App.java
@@ -0,0 +1,13 @@
+package com.kodilla.sudoku;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        boolean gameFinished = false;
+        while(!gameFinished) {
+            SudokuGame theGame = new SudokuGame();
+            //gameFinished = theGame.resolveSudoku();
+        }
+    }
+}

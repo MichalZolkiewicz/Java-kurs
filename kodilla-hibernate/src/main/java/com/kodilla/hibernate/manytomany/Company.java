94afe1b250752366d2ecf513dd54396d8b79a05a
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-03-28 20:12:11 +0200

    24.4 Course materials - Observer pattern used for Forum.

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @NamedQuery(
         name = "Company.retrieveCompanyNameByAnyNameFragment",
-        query = "FROM Company WHERE name LIKE '%' || :NAMEFRAGMENT ||'%' "
+        query = "FROM Company WHERE name LIKE concat('%',:NAMEFRAGMENT,'%')  "
 )
 @Entity
 @Table(name = "COMPANIES")
